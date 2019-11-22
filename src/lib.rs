43053d0b1ee7c6acf51250d3aff62430ebb02361
Daniël de Kok <me@danieldk.eu>
2019-11-22 12:45:16 +0100

    Implement TryFrom<Lines<R>> for WordPieces

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,6 @@
 use std::collections::HashSet;
+use std::convert::TryFrom;
+use std::io::{self, BufRead, Lines};
 
 /// A set of word pieces.
 pub struct WordPieces {
@@ -59,9 +61,36 @@ impl WordPieces {
     }
 }
 
+impl<R> TryFrom<Lines<R>> for WordPieces
+where
+    R: BufRead,
+{
+    type Error = io::Error;
+
+    fn try_from(lines: Lines<R>) -> Result<Self, Self::Error> {
+        let mut prefixes = HashSet::new();
+        let mut suffixes = HashSet::new();
+
+        for line in lines {
+            let line = line?;
+
+            if line.starts_with("##") {
+                suffixes.insert(line[2..].to_string());
+            } else {
+                prefixes.insert(line);
+            }
+        }
+
+        Ok(WordPieces { prefixes, suffixes })
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use std::collections::HashSet;
+    use std::convert::TryFrom;
+    use std::fs::File;
+    use std::io::{BufRead, BufReader};
     use std::iter::FromIterator;
 
     use super::WordPieces;
@@ -80,6 +109,20 @@ mod tests {
     #[test]
     fn test_word_pieces() {
         let word_pieces = example_word_pieces();
+
+        assert_eq!(word_pieces.split("voor"), Ok(vec!["voor".to_string()]));
+        assert_eq!(word_pieces.split("unknown"), Err(Vec::<String>::new()));
+        assert_eq!(word_pieces.split("voorman"), Err(vec!["voor".to_string()]));
+        assert_eq!(
+            word_pieces.split("coördinatie"),
+            Ok(vec!["coördina".to_string(), "##tie".to_string()])
+        );
+    }
+
+    #[test]
+    fn test_word_pieces_file() {
+        let f = File::open("testdata/test.pieces").unwrap();
+        let word_pieces = WordPieces::try_from(BufReader::new(f).lines()).unwrap();
 
         assert_eq!(word_pieces.split("voor"), Ok(vec!["voor".to_string()]));
         assert_eq!(word_pieces.split("unknown"), Err(Vec::<String>::new()));
