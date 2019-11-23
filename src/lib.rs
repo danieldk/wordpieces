f32b1c67c76ef8927f3f421c1379ffe7698d4dc0
Daniël de Kok <me@danieldk.eu>
2019-11-23 10:31:15 +0100

    Use finite state automata to make splitting O(n^2) -> O(n)
    The word piece splitting functionality was quadratic time. This change
    makes the splitting linear time by using finite state automata.
    
    The prefix and suffixes set are stored in dictionary automata. A new
    type, PrefixAutomaton is added. A prefix automaton matches every
    prefix in a string. E.g. for the string "Rust", it matches:
    
    [empty prefix]
    R
    Ru
    Rus
    Rust
    
    By computing the intersection language of a dictionary automaton and a
    prefix automaton, we can find the matching prefixes in the automaton
    in linear time. We then take the longest matching prefix as a word
    piece.
    
    While at it, also implement TryFrom to construct the finite state
    dictionaries from a file.

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,41 @@
-use std::collections::HashSet;
+use std::collections::BTreeSet;
+use std::convert::TryFrom;
+use std::io::{self, BufRead, Lines};
+
+use failure::Fail;
+use fst::{self, IntoStreamer, Set, SetBuilder, Streamer};
+
+mod automaton;
+use automaton::PrefixAutomaton;
+
+/// Errors that can occur while reading word pieces.
+#[derive(Debug, Fail)]
+pub enum WordPiecesError {
+    /// Finite state automaton error.
+    #[fail(display = "Finite state automaton error: {}", _0)]
+    FstError(fst::Error),
+
+    /// IO error.
+    #[fail(display = "IO error: {}", _0)]
+    IOError(io::Error),
+}
+
+impl From<io::Error> for WordPiecesError {
+    fn from(error: io::Error) -> Self {
+        WordPiecesError::IOError(error)
+    }
+}
+
+impl From<fst::Error> for WordPiecesError {
+    fn from(error: fst::Error) -> Self {
+        WordPiecesError::FstError(error)
+    }
+}
 
 /// A set of word pieces.
 pub struct WordPieces {
-    prefixes: HashSet<String>,
-    suffixes: HashSet<String>,
+    prefixes: Set,
+    suffixes: Set,
 }
 
 impl WordPieces {
@@ -11,75 +43,151 @@ impl WordPieces {
     ///
     /// The arguments are the prefix and suffix set. The suffix set
     /// should not have suffix markers (`##`).
-    pub fn new(prefixes: HashSet<String>, suffixes: HashSet<String>) -> Self {
+    pub fn new(prefixes: Set, suffixes: Set) -> Self {
         WordPieces { prefixes, suffixes }
     }
 
+    fn longest_prefix_len(affix_set: &Set, word: &str) -> usize {
+        let mut stream = affix_set.search(PrefixAutomaton::from(word)).into_stream();
+
+        let mut longest_len = match stream.next() {
+            Some(prefix) => prefix.len(),
+            None => return 0,
+        };
+
+        while let Some(prefix) = stream.next() {
+            if prefix.len() > longest_len {
+                longest_len = prefix.len()
+            }
+        }
+
+        longest_len
+    }
+
     /// Split a string into word pieces.
-    pub fn split(&self, word: &str) -> Result<Vec<String>, Vec<String>> {
-        // Get character offsets into `word`. Add the length of the
-        // word, for the upper bound of the string.
-        let mut char_indices: Vec<usize> = word.char_indices().map(|(idx, _)| idx).collect();
-        char_indices.push(word.len());
-
+    pub fn split(&self, mut word: &str) -> Result<Vec<String>, Vec<String>> {
         let mut pieces = Vec::new();
-        let mut begin = 0;
-        while begin < (char_indices.len() - 1) {
-            let mut end = char_indices.len() - 1;
 
-            while begin < end {
-                let candidate_piece = &word[char_indices[begin]..char_indices[end]];
+        // Find the word's prefix
+        let prefix_len = Self::longest_prefix_len(&self.prefixes, word);
+        if prefix_len == 0 {
+            return Err(pieces);
+        }
+        pieces.push(word[..prefix_len].to_string());
+        word = &word[prefix_len..];
 
-                if begin == 0 {
-                    // Prefix
-                    if self.prefixes.contains(candidate_piece) {
-                        pieces.push(candidate_piece.to_owned());
-                        break;
-                    }
-                } else {
-                    // Suffix
-                    if self.suffixes.contains(candidate_piece) {
-                        pieces.push(format!("##{}", candidate_piece.to_owned()));
-                        break;
-                    }
-                }
-
-                end -= 1;
-            }
-
-            if begin == end {
-                // No valid prefix could be found, return partial results.
+        // Find the word's suffixes.
+        while !word.is_empty() {
+            let prefix_len = Self::longest_prefix_len(&self.suffixes, word);
+            if prefix_len == 0 {
                 return Err(pieces);
-            } else {
-                begin = end;
             }
+
+            pieces.push(format!("##{}", word[..prefix_len].to_string()));
+            word = &word[prefix_len..];
         }
 
         Ok(pieces)
     }
 }
 
+impl<R> TryFrom<Lines<R>> for WordPieces
+where
+    R: BufRead,
+{
+    type Error = WordPiecesError;
+
+    fn try_from(lines: Lines<R>) -> Result<Self, Self::Error> {
+        let mut prefixes = BTreeSet::new();
+        let mut suffixes = BTreeSet::new();
+
+        for line in lines {
+            let line = line?;
+
+            if line.starts_with("##") {
+                suffixes.insert(line[2..].to_string());
+            } else {
+                prefixes.insert(line);
+            }
+        }
+
+        let mut prefix_set = SetBuilder::memory();
+        prefix_set.extend_iter(prefixes)?;
+
+        let mut suffix_set = SetBuilder::memory();
+        suffix_set.extend_iter(suffixes)?;
+
+        Ok(WordPieces {
+            prefixes: Set::from_bytes(prefix_set.into_inner()?)?,
+            suffixes: Set::from_bytes(suffix_set.into_inner()?)?,
+        })
+    }
+}
+
 #[cfg(test)]
 mod tests {
-    use std::collections::HashSet;
+    use std::collections::BTreeSet;
+    use std::convert::TryFrom;
+    use std::fs::File;
+    use std::io::{BufRead, BufReader};
     use std::iter::FromIterator;
+
+    use fst::{Set, SetBuilder};
 
     use super::WordPieces;
 
+    fn affixes_to_set(affixes: &[&str]) -> Set {
+        let affixes = BTreeSet::from_iter(affixes);
+        let mut builder = SetBuilder::memory();
+        builder.extend_iter(affixes).unwrap();
+        Set::from_bytes(builder.into_inner().unwrap()).unwrap()
+    }
+
     fn example_word_pieces() -> WordPieces {
         WordPieces {
-            prefixes: HashSet::from_iter(
-                (&["voor".to_string(), "coördina".to_string()])
-                    .iter()
-                    .map(Clone::clone),
-            ),
-            suffixes: HashSet::from_iter((&["tie".to_string()]).iter().map(Clone::clone)),
+            prefixes: affixes_to_set(&["voor", "coördina"]),
+            suffixes: affixes_to_set(&["tie", "kom", "en"]),
         }
     }
 
     #[test]
     fn test_word_pieces() {
         let word_pieces = example_word_pieces();
+
+        assert_eq!(word_pieces.split("voor"), Ok(vec!["voor".to_string()]));
+        assert_eq!(word_pieces.split("unknown"), Err(Vec::<String>::new()));
+        assert_eq!(word_pieces.split("voorman"), Err(vec!["voor".to_string()]));
+        assert_eq!(
+            word_pieces.split("coördinatie"),
+            Ok(vec!["coördina".to_string(), "##tie".to_string()])
+        );
+        assert_eq!(
+            word_pieces.split("voorkomen"),
+            Ok(vec![
+                "voor".to_string(),
+                "##kom".to_string(),
+                "##en".to_string()
+            ])
+        );
+    }
+
+    #[test]
+    fn longest_prefix_used() {
+        let word_pieces = WordPieces {
+            prefixes: affixes_to_set(&["foo", "fo"]),
+            suffixes: affixes_to_set(&["o", "bar", "b", "a", "r"]),
+        };
+
+        assert_eq!(
+            word_pieces.split("foobar"),
+            Ok(vec!["foo".to_string(), "##bar".to_string()])
+        );
+    }
+
+    #[test]
+    fn test_word_pieces_file() {
+        let f = File::open("testdata/test.pieces").unwrap();
+        let word_pieces = WordPieces::try_from(BufReader::new(f).lines()).unwrap();
 
         assert_eq!(word_pieces.split("voor"), Ok(vec!["voor".to_string()]));
         assert_eq!(word_pieces.split("unknown"), Err(Vec::<String>::new()));
