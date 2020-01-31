75367ca158535bcf9368b03992408ad6391af875
Daniël de Kok <me@danieldk.eu>
2020-01-31 13:50:01 +0100

    Provide vocab indices with word pieces that were found

--- src/word_pieces.rs
+++ src/word_pieces.rs
@@ -1,15 +1,15 @@
-use std::collections::BTreeSet;
+use std::collections::BTreeMap;
 use std::convert::TryFrom;
 use std::io::{BufRead, Lines};
 
-use fst::{self, IntoStreamer, Set, SetBuilder, Streamer};
+use fst::{self, IntoStreamer, Map, MapBuilder, Streamer};
 
 use crate::{PrefixAutomaton, WordPiecesError};
 
 /// A set of word pieces.
 pub struct WordPieces {
-    word_initial: Set,
-    continuation: Set,
+    word_initial: Map,
+    continuation: Map,
 }
 
 impl WordPieces {
@@ -18,28 +18,39 @@ impl WordPieces {
     /// The arguments are set of word-initial pieces and the set o
     /// continuation pieces. The continuation set pieces must not
     /// have continuation markers (such as `##`).
-    pub fn new(word_initial: Set, continuation: Set) -> Self {
+    pub fn new(word_initial: Map, continuation: Map) -> Self {
         WordPieces {
             word_initial,
             continuation,
         }
     }
 
-    fn longest_prefix_len(piece_set: &Set, word: &str) -> usize {
-        let mut stream = piece_set.search(PrefixAutomaton::from(word)).into_stream();
+    fn longest_prefix_len(piece_map: &Map, word: &str) -> (usize, u64) {
+        let mut stream = piece_map.search(PrefixAutomaton::from(word)).into_stream();
 
-        let mut longest_len = match stream.next() {
-            Some(prefix) => prefix.len(),
-            None => return 0,
+        let (mut longest_len, mut longest_idx) = match stream.next() {
+            Some((prefix, idx)) => (prefix.len(), idx),
+            None => return (0, 0),
         };
 
-        while let Some(prefix) = stream.next() {
+        while let Some((prefix, idx)) = stream.next() {
             if prefix.len() > longest_len {
-                longest_len = prefix.len()
+                longest_len = prefix.len();
+                longest_idx = idx;
             }
         }
 
-        longest_len
+        (longest_len, longest_idx)
+    }
+
+    /// Look up the index of an initial word piece.
+    pub fn get_continuation(&self, piece: &str) -> Option<u64> {
+        self.continuation.get(piece)
+    }
+
+    /// Look up the index of an continuation word piece.
+    pub fn get_initial(&self, piece: &str) -> Option<u64> {
+        self.word_initial.get(piece)
     }
 
     /// Split a string into word pieces.
@@ -61,28 +72,28 @@ where
     type Error = WordPiecesError;
 
     fn try_from(lines: Lines<R>) -> Result<Self, Self::Error> {
-        let mut word_initial = BTreeSet::new();
-        let mut continuation = BTreeSet::new();
+        let mut word_initial = BTreeMap::new();
+        let mut continuation = BTreeMap::new();
 
-        for line in lines {
+        for (idx, line) in lines.enumerate() {
             let line = line?;
 
             if line.starts_with("##") {
-                continuation.insert(line[2..].to_string());
+                continuation.insert(line[2..].to_string(), idx as u64);
             } else {
-                word_initial.insert(line);
+                word_initial.insert(line, idx as u64);
             }
         }
 
-        let mut word_initial_set = SetBuilder::memory();
+        let mut word_initial_set = MapBuilder::memory();
         word_initial_set.extend_iter(word_initial)?;
 
-        let mut continuation_set = SetBuilder::memory();
+        let mut continuation_set = MapBuilder::memory();
         continuation_set.extend_iter(continuation)?;
 
         Ok(WordPieces {
-            word_initial: Set::from_bytes(word_initial_set.into_inner()?)?,
-            continuation: Set::from_bytes(continuation_set.into_inner()?)?,
+            word_initial: Map::from_bytes(word_initial_set.into_inner()?)?,
+            continuation: Map::from_bytes(continuation_set.into_inner()?)?,
         })
     }
 }
@@ -91,17 +102,25 @@ where
 #[derive(Debug, Eq, PartialEq)]
 pub enum WordPiece<'a> {
     /// The next found word piece.
-    Found(&'a str),
+    Found { piece: &'a str, idx: u64 },
 
     /// No piece was found for the (remaining part of) the word.
     Missing,
 }
 
 impl<'a> WordPiece<'a> {
+    /// Unwrap an index if present.
+    pub fn idx(&self) -> Option<u64> {
+        match self {
+            WordPiece::Found { idx, .. } => Some(*idx),
+            WordPiece::Missing => None,
+        }
+    }
+
     /// Unwrap a piece if present.
     pub fn piece(&self) -> Option<&'a str> {
         match self {
-            WordPiece::Found(piece) => Some(piece),
+            WordPiece::Found { piece, .. } => Some(piece),
             WordPiece::Missing => None,
         }
     }
@@ -145,7 +164,7 @@ impl<'a, 'b> Iterator for WordPieceIter<'a, 'b> {
         };
 
         // Find the word's prefix in the set.
-        let prefix_len = WordPieces::longest_prefix_len(set, self.word);
+        let (prefix_len, prefix_idx) = WordPieces::longest_prefix_len(set, self.word);
         if prefix_len == 0 {
             // If there is no matching set, empty the word.
             self.word = &self.word[self.word.len()..];
@@ -156,33 +175,37 @@ impl<'a, 'b> Iterator for WordPieceIter<'a, 'b> {
 
         self.word = &self.word[prefix_len..];
 
-        Some(WordPiece::Found(piece))
+        Some(WordPiece::Found {
+            piece,
+            idx: prefix_idx,
+        })
     }
 }
 
 #[cfg(test)]
 mod tests {
-    use std::collections::BTreeSet;
+    use std::collections::BTreeMap;
     use std::convert::TryFrom;
     use std::fs::File;
     use std::io::{BufRead, BufReader};
     use std::iter::FromIterator;
 
-    use fst::{Set, SetBuilder};
+    use fst::{Map, MapBuilder};
 
     use super::{WordPiece, WordPieces};
 
-    fn pieces_to_set(pieces: &[&str]) -> Set {
-        let pieces = BTreeSet::from_iter(pieces);
-        let mut builder = SetBuilder::memory();
+    fn pieces_to_map(pieces: &[(&str, u64)]) -> Map {
+        let pieces =
+            BTreeMap::from_iter(pieces.iter().map(|(piece, idx)| (piece.to_string(), *idx)));
+        let mut builder = MapBuilder::memory();
         builder.extend_iter(pieces).unwrap();
-        Set::from_bytes(builder.into_inner().unwrap()).unwrap()
+        Map::from_bytes(builder.into_inner().unwrap()).unwrap()
     }
 
     fn example_word_pieces() -> WordPieces {
         WordPieces {
-            word_initial: pieces_to_set(&["voor", "coördina"]),
-            continuation: pieces_to_set(&["tie", "kom", "en"]),
+            word_initial: pieces_to_map(&[("voor", 0), ("coördina", 2)]),
+            continuation: pieces_to_map(&[("tie", 1), ("kom", 3), ("en", 4)]),
         }
     }
 
@@ -192,7 +215,10 @@ mod tests {
 
         assert_eq!(
             word_pieces.split("voor").collect::<Vec<_>>(),
-            vec![WordPiece::Found("voor")]
+            vec![WordPiece::Found {
+                piece: "voor",
+                idx: 0
+            }]
         );
         assert_eq!(
             word_pieces.split("unknown").collect::<Vec<_>>(),
@@ -200,18 +226,42 @@ mod tests {
         );
         assert_eq!(
             word_pieces.split("voorman").collect::<Vec<_>>(),
-            vec![WordPiece::Found("voor"), WordPiece::Missing]
+            vec![
+                WordPiece::Found {
+                    piece: "voor",
+                    idx: 0
+                },
+                WordPiece::Missing
+            ]
         );
         assert_eq!(
             word_pieces.split("coördinatie").collect::<Vec<_>>(),
-            vec![WordPiece::Found("coördina"), WordPiece::Found("tie")]
+            vec![
+                WordPiece::Found {
+                    piece: "coördina",
+                    idx: 2
+                },
+                WordPiece::Found {
+                    piece: "tie",
+                    idx: 1
+                }
+            ]
         );
         assert_eq!(
             word_pieces.split("voorkomen").collect::<Vec<_>>(),
             vec![
-                WordPiece::Found("voor"),
-                WordPiece::Found("kom"),
-                WordPiece::Found("en")
+                WordPiece::Found {
+                    piece: "voor",
+                    idx: 0,
+                },
+                WordPiece::Found {
+                    piece: "kom",
+                    idx: 3
+                },
+                WordPiece::Found {
+                    piece: "en",
+                    idx: 4
+                },
             ]
         );
     }
@@ -226,13 +276,22 @@ mod tests {
     #[test]
     fn longest_prefix_used() {
         let word_pieces = WordPieces {
-            word_initial: pieces_to_set(&["foo", "fo"]),
-            continuation: pieces_to_set(&["o", "bar", "b", "a", "r"]),
+            word_initial: pieces_to_map(&[("foo", 0), ("fo", 2)]),
+            continuation: pieces_to_map(&[("o", 1), ("bar", 3), ("b", 4), ("a", 5), ("r", 6)]),
         };
 
         assert_eq!(
             word_pieces.split("foobar").collect::<Vec<_>>(),
-            vec![WordPiece::Found("foo"), WordPiece::Found("bar")]
+            vec![
+                WordPiece::Found {
+                    piece: "foo",
+                    idx: 0
+                },
+                WordPiece::Found {
+                    piece: "bar",
+                    idx: 3
+                }
+            ]
         );
     }
 
@@ -243,7 +302,10 @@ mod tests {
 
         assert_eq!(
             word_pieces.split("voor").collect::<Vec<_>>(),
-            vec![WordPiece::Found("voor")]
+            vec![WordPiece::Found {
+                piece: "voor",
+                idx: 0
+            }]
         );
         assert_eq!(
             word_pieces.split("unknown").collect::<Vec<_>>(),
@@ -251,11 +313,26 @@ mod tests {
         );
         assert_eq!(
             word_pieces.split("voorman").collect::<Vec<_>>(),
-            vec![WordPiece::Found("voor"), WordPiece::Missing]
+            vec![
+                WordPiece::Found {
+                    piece: "voor",
+                    idx: 0
+                },
+                WordPiece::Missing
+            ]
         );
         assert_eq!(
             word_pieces.split("coördinatie").collect::<Vec<_>>(),
-            vec![WordPiece::Found("coördina"), WordPiece::Found("tie")]
+            vec![
+                WordPiece::Found {
+                    piece: "coördina",
+                    idx: 2
+                },
+                WordPiece::Found {
+                    piece: "tie",
+                    idx: 1
+                }
+            ]
         );
     }
 }
