ecddb30724c18e0ec42fcc1a43761f7bd9f89e54
Daniël de Kok <me@danieldk.eu>
2020-04-15 12:15:15 +0200

    Use `thiserror` to derive errors

--- src/error.rs
+++ src/error.rs
@@ -1,27 +1,15 @@
 use std::io;
 
-use failure::Fail;
+use thiserror::Error;
 
 /// Errors that can occur while reading word pieces.
-#[derive(Debug, Fail)]
+#[derive(Debug, Error)]
 pub enum WordPiecesError {
     /// Finite state automaton error.
-    #[fail(display = "Finite state automaton error: {}", _0)]
-    FstError(fst::Error),
+    #[error(transparent)]
+    FstError(#[from] fst::Error),
 
     /// IO error.
-    #[fail(display = "IO error: {}", _0)]
-    IOError(io::Error),
-}
-
-impl From<io::Error> for WordPiecesError {
-    fn from(error: io::Error) -> Self {
-        WordPiecesError::IOError(error)
-    }
-}
-
-impl From<fst::Error> for WordPiecesError {
-    fn from(error: fst::Error) -> Self {
-        WordPiecesError::FstError(error)
-    }
+    #[error(transparent)]
+    IOError(#[from] io::Error),
 }
